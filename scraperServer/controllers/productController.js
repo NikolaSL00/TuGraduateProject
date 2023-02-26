87166e3cb6867c903944998bc5cbfbe8147bfca6
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2023-02-26 18:46:44 +0200

    decrypted python script

--- scraperServer/controllers/productController.js
+++ scraperServer/controllers/productController.js
@@ -1,20 +1,10 @@
 const router = require("express").Router();
-// const { isAuth } = require("../middlewares/authMiddleware");
+const { decrypt } = require("../middlewares/decryptMiddlware");
 const storeService = require("../services/storeService");
 const productService = require("../services/productService");
 
 const { getErrorMessage } = require("../utils/errorHelpers");
 
-router.post("/", async (req, res) => {
-  const { email } = req.body;
-
-  try {
-    console.log(req.body);
-  } catch (error) {
-    console.log(getErrorMessage(error));
-    // mongoose error
-    // return res.status(400).send({ error: getErrorMessage(error) });
-  }
-});
+router.post("/", decrypt, async (req, res) => {});
 
 module.exports = router;
