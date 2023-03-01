edfb9a7a0d4a82e4fe53bb6a3f9040c624dc1c79
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2023-03-01 16:07:39 +0200

    added https on the rest of the servers

--- dbServer/routes.js
+++ dbServer/routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const productController = require("./controllers/productController");
+// const productController = require("./controllers/productController");
 
-router.use("/data", productController);
+// router.use("/data", productController);
 
 module.exports = router;
