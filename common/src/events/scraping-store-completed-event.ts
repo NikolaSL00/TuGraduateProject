f17161e6868fe4aec7e92b5da83dce3c9bdcd88a
blame Nikola Slavchev <nslavchev48@gmail.com>
2023-04-19 21:32:41 +0300

    Updates

--- common/src/events/scraping-store-completed-event.ts
+++ common/src/events/scraping-store-completed-event.ts
@@ -3,14 +3,17 @@ import { Subjects } from './subjects';
 export interface ScrapingStoreCompletedEvent {
   subject: Subjects.ScrapingStoreCompleted;
 
-  data: [
-    {
-      title: string;
-      description: string;
-      imageUrl: string;
-      price: string;
-      unit: string;
-      productUrl: string;
-    }
-  ];
+  data: {
+    name: string;
+    products: [
+      {
+        title: string;
+        description: string;
+        imageUrl: string;
+        price: string;
+        unit: string;
+        productUrl: string;
+      }
+    ];
+  };
 }
