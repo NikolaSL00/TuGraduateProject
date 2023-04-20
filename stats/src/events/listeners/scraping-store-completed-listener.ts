746b75b289b100828ee43587afc1b2f0ca4232f3
blame Nikola Slavchev <nslavchev48@gmail.com>
2023-04-20 09:34:23 +0300

    implement event listener on stats

---
+++ stats/src/events/listeners/scraping-store-completed-listener.ts
@@ -0,0 +1,20 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Subjects,
+  Listener,
+  ScrapingStoreCompletedEvent,
+} from '@shopsmart/common';
+import { queueGroupName } from './queue-group-name';
+
+export class ScrapingStoreCompletedListener extends Listener<ScrapingStoreCompletedEvent> {
+  readonly subject = Subjects.ScrapingStoreCompleted;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ScrapingStoreCompletedEvent['data'], msg: Message) {
+    console.log('Data received: ', data.name);
+
+    console.log(data.products.length);
+
+    msg.ack();
+  }
+}
