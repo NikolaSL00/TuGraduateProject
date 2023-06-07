c0ce66ee898ddec74860b0848594fd5bb7fe8609
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2023-06-07 08:38:06 +0300

    increase event bus message payload to 32MB

--- scraping/src/scheduler/jobs/test.js
+++ scraping/src/scheduler/jobs/test.js
@@ -1,24 +1,35 @@
 const { parentPort } = require("worker_threads");
 
 (async () => {
-    let products = [
-        {
-          title: 'title1',
-          description: 'description1',
-          imageUrl: 'imageUrl1',
+
+  function getMemorySizeOfObject(obj) {
+    const jsonString = JSON.stringify(obj);
+    return new Blob([jsonString]).size;
+  }
+
+  const obj1 = {
+    title: 'title1',
+    description: 'description1',
+    imageUrl: 'imageUrl1',
+    price: '12.12',
+    unit: 'kg',
+    productUrl: 'productUrl1',
+  }
+
+  const limit = 100_000;
+  console.log("Approximately memory size of 1 object: ", getMemorySizeOfObject(obj1));
+    let products = [];
+
+      for (let i = 0; i <= limit; i++ ){
+        products.push({
+          title: `title${i}`,
+          description: `description${i}`,
+          imageUrl: `imageUrl${i}`,
           price: '12.12',
           unit: 'kg',
-          productUrl: 'productUrl1',
-        },
-        {
-            title: 'title2',
-            description: 'description2',
-            imageUrl: 'imageUrl2',
-            price: '13.13',
-            unit: 'kg',
-            productUrl: 'productUrl2',
-          },
-      ];
+          productUrl: `productUrl${i}`,
+        });
+      }
 
   try {
     parentPort.postMessage({
@@ -36,7 +47,6 @@ const { parentPort } = require("worker_threads");
     parentPort.postMessage({ error: err });
   }
   finally {
-    await browser.close();
     process.exit(0);
 }
 })();
--- scraping/src/scheduler/scheduler.js
+++ scraping/src/scheduler/scheduler.js
@@ -22,28 +22,28 @@ export const scheduler = () => {
       //     isPhysical: false,
       //   },
       // ],
-      name: "bulmagShumen",
+      name: "zasiti",
       // cron: '0 5,17 * * *', // runs the task twice a day at 5AM and 5 PM
       // timeout: 1200000, // 20 minutes
       interval: "60s",
       timeout: "25s",
-      worker: `bulmagShumen.js`,
+      worker: `zasiti.js`,
       // interval: "5s",
     },
   ];
 
   function myWorkerMessageHandler(job) {
 
-    console.log(job.message.error);
+    // console.log(job.message.error);
     if(job.message.error){
-      console.log('error: ');
-      console.log(job.name);
-      console.log(job.message.error);
+      // console.log('error: ');
+      // console.log(job.name);
+      // console.log(job.message.error);
     } else {
-      console.log('finished');
-      console.log(job.name);
-      console.log(job.message.locations);
-      console.log(job.message.result);
+      // console.log('finished');
+      // console.log(job.name);
+      // console.log(job.message.locations);
+      console.log(job.message.result.length);
     }
     
 
