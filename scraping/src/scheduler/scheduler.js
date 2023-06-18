718f4287ffe12fb85bd0940d0eded3e623830488
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2023-06-18 17:28:59 +0300

    fuzzy search for basic product search

--- scraping/src/scheduler/scheduler.js
+++ scraping/src/scheduler/scheduler.js
@@ -13,42 +13,42 @@ export const scheduler = () => {
     // same thing at 1PM ...
     {
       name: "minamart",
-      cron: "0 0,12 * * *", // starts at 9 AM and 9 PM
+      cron: "0 0,12 * * *", // starts at 3 AM and 3 PM
       worker: `minamart.js`,
     },
     {
       name: "gastronom",
-      cron: "0 1,13 * * *", // starts at 10 AM and 10 PM
+      cron: "0 1,13 * * *", // starts at 4 AM and 4 PM
       worker: `gastronom.js`,
     },
     {
       name: "shopGladen",
-      cron: "0 2,14 * * *", // starts at 11 AM and 11 PM
+      cron: "0 2,14 * * *", // starts at 5 AM and 5 PM
       worker: `shopGladen.js`,
     },
     {
       name: "ebag",
-      cron: "0 4,16 * * *", // starts at 1 AM and 1 PM
+      cron: "0 4,16 * * *", // starts at 6 AM and 6 PM
       worker: `ebag.js`,
     },
     {
       name: "parkmartSofia",
-      cron: "0 6,18 * * *", // starts at 3 AM and 3 PM
+      cron: "0 6,18 * * *", // starts at 9 AM and 9 PM
       worker: `parkmartSofia.js`,
     },
     {
       name: "parkmartVarna",
-      cron: "0 8,20 * * *", // starts at 5 AM and 5 PM
+      cron: "0 8,20 * * *", // starts at 11 AM and 11 PM
       worker: `parkmartVarna.js`,
     },
     {
       name: "parkmartBurgas",
-      cron: "0 10,22 * * *", // starts at 7 AM and 7 PM
+      cron: "0 10,22 * * *", // starts at 1 AM and 1 PM
       worker: `parkmartBurgas.js`,
     },
     {
       name: "zasiti",
-      cron: "0 11,23 * * *", // starts at 8 AM and 8 PM
+      cron: "0 11,23 * * *", // starts at 2 AM and 2 PM
       worker: `zasiti.js`,
     },
   ];
