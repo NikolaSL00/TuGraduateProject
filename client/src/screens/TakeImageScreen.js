8123c9a67d59ff7962ac51eef10d7ed59f0d4c7e
Nikola Slavchev <78145211+NikolaSL00@users.noreply.github.com>
2023-06-23 11:08:00 +0300

    image recognition model set up

--- client/src/screens/TakeImageScreen.js
+++ client/src/screens/TakeImageScreen.js
@@ -1,10 +1,50 @@
-import {View, Text} from 'react-native';
+// import * as tf from "@tensorflow/tfjs";
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, Platform } from "react-native";
+// import * as ImagePicker from "expo-image-picker";
+
+// async function loadModel() {
+//   await tf.ready();
+//   const model = await tf.loadGraphModel("../image-rec-model/model.json");
+//   console.log("model is loaded");
+//   return model;
+// }
 
 const TakeImageScreen = () => {
+  // const [model, setModel] = useState(undefined);
 
-    return <View>
-        <Text>TakeImageScreen</Text>
+  // useEffect(() => {
+  //   loadModel()
+  //     .then((model) => setModel((_) => model))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  const [image, setImage] = useState(null);
+
+  // const pickImage = async () => {
+  //   // No permissions request is necessary for launching the image library
+  //   let result = await ImagePicker.launchImageLibraryAsync({
+  //     mediaTypes: ImagePicker.MediaTypeOptions.All,
+  //     allowsEditing: true,
+  //     aspect: [4, 3],
+  //     quality: 1,
+  //   });
+
+  //   console.log(result);
+
+  //   if (!result.canceled) {
+  //     setImage(result.assets[0].uri);
+  //   }
+  // };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
-}
+  );
+};
 
 export default TakeImageScreen;
